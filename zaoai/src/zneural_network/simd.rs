5e3efaca84a71291e7256948d3f02c4c46a7caa4
FjZ345 <fjz345@hotmail.com>
2026-09-04 21:50:57 +0200

    Removed cfg simd as it is already on mod simd.rs

--- zaoai/src/zneural_network/simd.rs
+++ zaoai/src/zneural_network/simd.rs
@@ -1,24 +1,18 @@
-#[cfg(feature = "simd")]
-use wide::f32x8;
-
-#[cfg(feature = "simd")]
 use crate::zneural_network::activation::relu_d;
+use wide::f32x8;
 
 // ============================
 // Cost Functions
 // ============================
-#[cfg(feature = "simd")]
 pub fn mse_simd(output_activation: f32x8, expected_activation: f32x8) -> f32x8 {
     let error = output_activation - expected_activation;
     // 0.5 * error^2
     f32x8::splat(0.5) * error * error
 }
-#[cfg(feature = "simd")]
 pub fn mse_d_simd(output_activation: f32x8, expected_activation: f32x8) -> f32x8 {
     output_activation - expected_activation
 }
 
-#[cfg(feature = "simd")]
 pub fn cross_entropy_loss_multiclass_simd(predicted: f32x8, expected: f32x8) -> f32x8 {
     let epsilon = f32x8::splat(1e-12);
     let one = f32x8::splat(1.0);
@@ -26,13 +20,10 @@ pub fn cross_entropy_loss_multiclass_simd(predicted: f32x8, expected: f32x8) -> 
     let clamped = predicted.min(one - epsilon).max(epsilon);
     -expected * clamped.ln()
 }
-#[cfg(feature = "simd")]
 pub fn cross_entropy_loss_multiclass_d_simd(predicted: f32x8, expected: f32x8) -> f32x8 {
     // Assumes inputs are after softmax
     predicted - expected
 }
-
-#[cfg(feature = "simd")]
 pub fn cross_entropy_loss_binary_simd(predicted: f32x8, expected: f32x8) -> f32x8 {
     let epsilon = f32x8::splat(1e-12);
     let one = f32x8::splat(1.0);
@@ -41,7 +32,6 @@ pub fn cross_entropy_loss_binary_simd(predicted: f32x8, expected: f32x8) -> f32x
 
     -(expected * clamped.ln() + (one - expected) * (one - clamped).ln())
 }
-#[cfg(feature = "simd")]
 pub fn cross_entropy_loss_binary_d_simd(predicted: f32x8, expected: f32x8) -> f32x8 {
     let epsilon = f32x8::splat(1e-12);
     let one = f32x8::splat(1.0);
@@ -59,25 +49,18 @@ pub fn cross_entropy_loss_binary_d_simd(predicted: f32x8, expected: f32x8) -> f3
 // ============================
 // Activation Functions
 // ============================
-#[cfg(feature = "simd")]
 pub fn sigmoid_simd(x: f32x8) -> f32x8 {
     let one = f32x8::splat(1.0);
     one / (one + (-x).exp())
 }
-
-#[cfg(feature = "simd")]
 pub fn sigmoid_d_simd(x: f32x8) -> f32x8 {
     let fx = sigmoid_simd(x);
     fx * (f32x8::splat(1.0) - fx)
 }
-
-#[cfg(feature = "simd")]
 pub fn relu_simd(in_value: f32x8) -> f32x8 {
     // Fastmax?
     in_value.max(f32x8::splat(0.0))
 }
-
-#[cfg(feature = "simd")]
 pub fn relu_d_simd(x: f32x8) -> f32x8 {
     // temp fix
     let a: Vec<f32> = x.to_array().iter_mut().map(|f| relu_d(*f)).collect();
@@ -87,5 +70,4 @@ pub fn relu_d_simd(x: f32x8) -> f32x8 {
     // use wide::CmpGt;
     // x.cmp_gt(f32x8::splat(0.0))
 }
-
 // ============================
